41edcc347a640d19754d1e32d6d920c8b755ce05
Paul Vidal <pvandriod93@gmail.com>
2024-08-23 10:33:11 -0500

    Conection from msa-customer to config server

---
+++ devsu-source/service/config-server/src/test/java/com/devsu/service/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsu.service.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
